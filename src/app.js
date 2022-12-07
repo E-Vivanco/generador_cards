4f18cf708decbde2bf8afe920fe52344ee11a329
E-Vivanco <vivanco.elsa@gmail.com>
2022-12-07 19:41:44 +0000

    6° act gen-card

--- src/app.js
+++ src/app.js
@@ -7,5 +7,28 @@ import "./assets/img/4geeks.ico";
 
 window.onload = function() {
   //write your code here
-  console.log("Hello Rigo from the console!");
+  let palo1 = document.querySelector("#paloI");
+  let paloizq = palo1.children[0].children[0];
+  paloizq.addEventListener("click", function() {
+    ubicar_palo_i();
+  });
+  //console.log("paloizq va aqui", paloizq);
+  let palo2 = document.querySelector("#paloD");
+  let paloder = palo2.children[0].children[0];
+  paloder.addEventListener("click", function() {
+    ubicar_palo_d();
+  });
+  //console.log("paloder va aqui", paloder);
+  let num_random = document.querySelector("#numero_r");
+  let numero = num_random.children[0].children[0];
+  numero.addEventListener("click", function() {
+    num_rand();
+  });
+  //console.log("El numero va aqui", numero);
+  //console.log(num_random.children[0].children[0]);
+  //  num_rand();
+  //console.log(palo2.children[0].children[0]);
+  //let paloder = palo2.children[0].children[1];
+  //console.log(paloder);
+  //console.log("Hello Rigo from the console!");
 };
