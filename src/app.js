b936359954ab5b16b9fadecb9a7b86d2d1c0a4b1
E-Vivanco <vivanco.elsa@gmail.com>
2022-12-07 20:09:20 +0000

    10° act gen-card

--- src/app.js
+++ src/app.js
@@ -10,23 +10,29 @@ window.onload = function() {
   //console.log("paloizq va aqui", paloizq);
   let palo1 = document.querySelector("#paloI");
   let paloizq = palo1.children[0].children[0];
-  paloizq.addEventListener("click", function() {
+  paloizq.addEventListener("onload", function() {
     ubicar_palo_i(paloizq);
   });
 
   //console.log("paloder va aqui", paloder);
   let palo2 = document.querySelector("#paloD");
   let paloder = palo2.children[0].children[0];
-  paloder.addEventListener("click", function() {
+  paloder.addEventListener("onload", function() {
     ubicar_palo_d();
   });
 
   //console.log("El numero va aqui", numero);
   let num_random = document.querySelector("#numero_r");
   let numero1 = num_random.children[0].children[0];
-  numero1.addEventListener("click", function() {
+  numero1.addEventListener("onload", function() {
     number();
   });
+
+  //function ubicar_palo_i(val) {
+  //  p_iz = Math.floor(Math.random() * 10 + 1);
+  //  val = paloizq.appendChild(p_iz);
+  //  console.log(val);
+  //}
 };
 function ubicar_palo_i(val) {
   p_iz = Math.floor(Math.random() * 10 + 1);
