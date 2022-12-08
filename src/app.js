55cffca051b8e15517eb5837c5efbefcdf2d86b3
E-Vivanco <vivanco.elsa@gmail.com>
2022-12-08 19:08:06 +0000

    18° act gen-cards

--- src/app.js
+++ src/app.js
@@ -16,25 +16,15 @@ function generarpalos() {
   let tipo = parseInt(Math.floor(Math.random() * 3 + 1));
   return cards1[tipo];
 }
-//console.log(generarpalos())
-let var1 = document.querySelector("#paloI .palo_izq");
-var1.innerHTML = generarpalos();
-let var3 = document.querySelector("#numero_r .numero");
-var3.innerHTML = generaCartas();
-//document.body.appendChild(var3);
-//console.log("aqui var4", var4);
-//console.log("aqui la var3", var3);
-//console.log(document.body.appendChild(var3));
-let var2 = document.querySelector("#paloD .palo_der");
-var2.innerHTML = generarpalos();
 
+//function cargar_cards() {}
 window.onload = function() {
   document.querySelector("#filtro_cards").addEventListener("click", () => {
-    //var1.innerHTML = generarpalos();
-    document.body.appendChild(var1);
-    //var2.innerHTML = generarpalos();
-    document.body.appendChild(var2);
-    //var3.innerHTML = generaCartas();
-    document.body.appendChild(var3);
+    document.querySelectorAll(
+      "#paloI .palo_izq",
+      "#paloD .palo_der"
+    ).innerHTML = generarpalos();
+    document.querySelector("#numero_r .numero").innerHTML = generaCartas();
+    // document.querySelector("#paloD .palo_der").innerHTML = generarpalos();
   });
 };
